d6cc8229c101eb07467c2be2d40cc78b67c908b8
HalcyonsDev <halcyon.ruslan@gmail.com>
2023-09-16 20:25:53 +0300

    added post dto

---
+++ src/main/java/com/halcyon/julioimage/dto/post/NewPostDto.java
@@ -0,0 +1,17 @@
+package com.halcyon.julioimage.dto.post;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewPostDto {
+    @Size(min = 1, max = 50, message = "Name must be more than 1 character and less than 50 characters.")
+    private String name;
+
+    @Size(min = 3, max = 64, message = "Description must be more than 3 characters and less than 64 characters.")
+    private String description;
+}
