229fbf66dfd24bb35cc1e2e2f991d570cb587e1e
HalcyonsDev <halcyon.ruslan@gmail.com>
2023-09-16 20:20:30 +0300

    init

---
+++ src/main/java/com/halcyon/julioimage/JulioImageApplication.java
@@ -0,0 +1,13 @@
+package com.halcyon.julioimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulioImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulioImageApplication.class, args);
+	}
+
+}
